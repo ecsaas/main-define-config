1890ccfb27af8656d67fbf2de9051dd48da155fd
manhbv.giaminh <manhbv.giaminh@gmail.com>
2022-10-27 14:40:51 +0700

    config header clear browser data

--- mdc/header-value.go
+++ mdc/header-value.go
@@ -7,3 +7,6 @@ const ALLOW_ORIGIN_VALUE = "*"
 const ALLOW_METHODS_VALUE = "POST, GET, OPTIONS, PUT, PATCH, DELETE"
 
 const ALLOW_HEADERS_VALUE = "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"
+const STRICT_TRANSPORT_VALUE = "max-age=31536000; includeSubDomains; preload"
+const CLEAR_SITE_VALUE_ALL = `"*"`
+const CLEAR_SITE_VALUE_DEFAULT = `"cache", "cookies", "storage", "executionContexts"`
